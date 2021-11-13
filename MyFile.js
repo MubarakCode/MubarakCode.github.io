f483ecba7eee2c174fe9c939396efc62abbbccad
Mubarak Suleiman <suleimanomubarak@gmail.com>
2021-11-13 18:32:32 +0100

    completion
    completion

---
+++ MyFile.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var element = document.getElementsById("btn-1");
+    element.classList.add("btn-1");
+  }
+
+function scrollto(where) {
+    var scrollDiv = document.getElementById(where).offsetTop;
+    
+    window.scrollTo({ top: scrollDiv, behavior: 'smooth'});
+}
