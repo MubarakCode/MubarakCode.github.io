aa4b4b99765ab8c0510969597733476a4d416e0e
Mubarak Suleiman <44719922+MubarakCode@users.noreply.github.com>
2024-11-27 21:51:18 +0000

    Add files via upload

--- MyFile.js
+++ MyFile.js
@@ -7,3 +7,18 @@ function scrollto(where) {
     var scrollDiv = document.getElementById(where).offsetTop;
     window.scrollTo({ top: scrollDiv, behavior: 'smooth'});
 }
+
+
+window.onscroll = function() {toggleHeader()};
+
+function toggleHeader() {
+    let pos = window.scrollY;
+
+    var header = document.getElementById('header')
+
+    if (pos < 200) {
+        header.classList.remove('fixedheader')
+    } else {
+        header.classList.add('fixedheader')
+    }
+}
